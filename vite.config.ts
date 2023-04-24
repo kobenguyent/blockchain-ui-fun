854eea2855b80c8b3fc7fefc8eba0b768a596abf
kobenguyent <kobenguyent@gmail.com>
2023-04-24 14:20:31 +0200

    fix: asset path

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/blockchain-ui-fun/'
+  base: '/blockchain-ui-fun/',
+  assetsInclude: ['**/*.png'],
 })
